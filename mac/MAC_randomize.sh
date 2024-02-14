8d22e71ad28f47c2f15a0a39477891db91143f74
Samuel <elsamuko@gmail.com>
2024-02-14 08:26:21 +0100

    fix: MAC_randomize.sh

--- mac/MAC_randomize.sh
+++ mac/MAC_randomize.sh
@@ -8,11 +8,12 @@ MAC1="$(openssl rand -hex 6 | sed 's/\(..\)/\1:/g; s/.$//')"
 
 # -z "Disassociate from any network"
 sudo /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport en0 -z
+sudo /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport awdl0 -z
 
 echo "Setting en0 MAC address to $MAC0"
 sudo ifconfig en0 ether "$MAC0"
 sudo ifconfig en0 lladdr "$MAC0"
 
-# echo "Setting en1 MAC address to $MAC1"
-# sudo ifconfig en1 ether "$MAC1"
-# sudo ifconfig en1 lladdr "$MAC1"
+echo "Setting awdl0 MAC address to $MAC1"
+sudo ifconfig awdl0 ether "$MAC1"
+sudo ifconfig awdl0 lladdr "$MAC1"
