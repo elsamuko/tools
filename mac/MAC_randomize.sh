44c8dd388cc697ee84c84223184a6129d49bbc01
Samuel <elsamuko@gmail.com>
2026-08-07 15:53:45 +0200

    chg: updated MAC address changer

--- mac/MAC_randomize.sh
+++ mac/MAC_randomize.sh
@@ -3,17 +3,21 @@
 # https://www.howtogeek.com/220462/how-to-find-and-change-your-mac-address-on-os-x/
 # https://djangocas.dev/blog/mac/how-to-change-mac-network-mac-address/
 
-MAC0="$(openssl rand -hex 6 | sed 's/\(..\)/\1:/g; s/.$//')"
-MAC1="$(openssl rand -hex 6 | sed 's/\(..\)/\1:/g; s/.$//')"
+# exit on error
+set -e
+
+MAC0="02:$(openssl rand -hex 5 | sed 's/\(..\)/\1:/g; s/.$//')"
+MAC1="02:$(openssl rand -hex 5 | sed 's/\(..\)/\1:/g; s/.$//')"
 
 # -z "Disassociate from any network"
-sudo /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport en0 -z
-sudo /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport awdl0 -z
+sudo networksetup  -setairportpower en0 off
+sudo networksetup  -setairportpower en0 on
 
 echo "Setting en0 MAC address to $MAC0"
+sudo ifconfig en0 ether
+echo "======================="
 sudo ifconfig en0 ether "$MAC0"
-sudo ifconfig en0 lladdr "$MAC0"
+sudo ifconfig en0 ether
+echo "======================="
 
-echo "Setting awdl0 MAC address to $MAC1"
-sudo ifconfig awdl0 ether "$MAC1"
-sudo ifconfig awdl0 lladdr "$MAC1"
+sudo networksetup  -detectnewhardware
